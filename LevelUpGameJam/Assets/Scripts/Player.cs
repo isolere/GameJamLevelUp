226634e0a3a92e5381d0965ad975145e35d45e5a
isolere <isoler48@gmail.com>
2023-07-20 16:20:48 +0200

    arreglar colliders

--- LevelUpGameJam/Assets/Scripts/Player.cs
+++ LevelUpGameJam/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -8,6 +9,13 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed = 205f;
     [SerializeField] float slowSpeed = 15f;
     [SerializeField] float boostSpeed = 30f;
+
+
+    private bool retrocedir = false;
+
+    private float timer = 0.0f;
+    private float duracio = 0.3f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +25,10 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        //how much I am steering every single frame, it needs to be updating and changing
+    {        //how much I am steering every single frame, it needs to be updating and changing
+
+        if (!retrocedir) { 
+
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
@@ -27,12 +37,40 @@ public class Player : MonoBehaviour
         transform.Translate(moveAmount, 0, 0);
     }
 
+        if (retrocedir)
+        {
+            // Desplaçament cap enrere durant un segon
+
+            transform.Translate(-100 * Time.deltaTime, -0, 0);
+            timer += Time.deltaTime;
+
+            if (timer >= duracio)
+            {
+                // Recuperar les condicions originals
+                float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+                float moveAmount = Input.GetAxis("Vertical") * 200 * Time.deltaTime;
+
+                transform.Rotate(0, steerAmount, 0);
+
+                transform.Translate(moveAmount, 0, 0);
+                retrocedir = false;
+                timer = 0f;
+            }
+        }
+
+
+    }
+
 
     void OnCollisionEnter(Collision other)
     {
+        if (other.gameObject.tag != "pla_collider_aigua")
+        {
+             retrocedir = true;
+        }
+
         //moveSpeed = slowSpeed; 
     }
-
 
 
     void OnTriggerEnter(Collider other)
@@ -43,7 +81,6 @@ public class Player : MonoBehaviour
             Debug.Log("Obtens un Boost!");
 
         }
-
 
     }
 }
